28cfafdcf07a31ead0a6c78aa1c2f3f6e1ebe073
Samuel Njenga <snjenga704@gmail.com>
2020-06-25 15:44:08 +0300

    Updated BikeRoutes.js by modifying the roles middleware in the get endpoints

--- routes/BikeRoutes.js
+++ routes/BikeRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/RegLoginController');
 const bikeController = require('../controllers/BikeController');
 
-router.get('/bikes', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), bikeController.getBikes);
+router.get('/bikes',bikeController.getBikes);
 
 router.post('/bike', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), bikeController.postBike);
 
-router.get('/getDescription/:_id', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), bikeController.getDescription);
+router.get('/description/:_id',bikeController.getDescription);
 
 router.delete('/bike/:postId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), bikeController.deleteBike);
 
